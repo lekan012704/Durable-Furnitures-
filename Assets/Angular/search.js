94da5bc0f0c80aa48287a2dfc4314cf7f2a871ed
Durosinmi Abdul Sobuur <durosinmiabdulsobuur@gmail.com>
2021-07-15 16:58:16 +0100

    Initial

---
+++ Assets/Angular/search.js
@@ -0,0 +1,5 @@
+var app = angular.module("myApp",[]);
+       app.controller("myCtrl",function($scope)
+          {
+              $scope.names =['AGATHAS INTERIOR DESIGN', 'ADERAZ BENSON','ANTHUGHBURO TIME LIMITED','ALNO ASSOCIATES','AUKEN INTERNATIONAL PRODUCT','LIFEMATE NIGERIA FURNITURE LIMITED','BEDMATE FURNITURE NIGERIA'];
+          });
